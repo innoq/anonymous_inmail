5ac62d29d1142b46c60f1b46898108debeb56136
Andreas Krüger <andreas.krueger@innoq.com>
2018-01-17 19:35:19 +0100

    We now have a form and an answer.

--- src/ano_inbox/app.py
+++ src/ano_inbox/app.py
@@ -1,6 +1,15 @@
-from flask import Flask
+import sys
+from flask import Flask, render_template, request
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    return render_template('index.html')
+
+@app.route("/submit", methods=['POST'])
+def submit():
+    d = request.form
+    sys.stderr.write("Have a POST request\n")
+    for k in d.keys():
+        sys.stderr.write("{} -> {}\n".format(k, d.getlist(k)))
+    return render_template('sent.html')
